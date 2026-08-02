10a8a10343f71a935d7197dc80dc8fcf161407ca
Jaden-JJH <73709050+Jaden-JJH@users.noreply.github.com>
2026-08-02 14:36:29 +0900

    fix: Code String Concat (CWE-95), Eval Detected (CWE-95)

--- app/routes/contributions.js
+++ app/routes/contributions.js
@@ -33,11 +33,10 @@ function ContributionsHandler(db) {
                 const afterTax = parseFloat(req.body.afterTax) || 0;
                 const roth = parseFloat(req.body.roth) || 0;
         /*
-        //Fix for A1 -1 SSJS Injection attacks - uses alternate method to eval
-        const preTax = parseInt(req.body.preTax);
-        const afterTax = parseInt(req.body.afterTax);
-        const roth = parseInt(req.body.roth);
-        */
+        //Fix for A1 -1 SSJS Injection attacks - uses alternate m// Safe parsing of numeric inputs
+        const preTax = parseInt(req.body.preTax, 10);
+        const afterTax = parseInt(req.body.afterTax, 10);
+        const roth = parseInt(req.body.roth, 10);    */
         const {
             userId
         } = req.session;
